c22c34e43a61d523499c1df3838e4bded7fc9158
Moises Velasquez <mvelasquez@pictoric.com>
2022-06-21 21:53:31 -0500

    import reactive module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormModule } from './reactive-form/reactive-form.module';
 import { TemplateFormModule } from './template-form/template-form.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, TemplateFormModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TemplateFormModule,
+    ReactiveFormModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
