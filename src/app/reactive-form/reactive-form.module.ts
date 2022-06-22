f29a2e201d26ad6c7cbceae80b47cae2008ed704
Moises Velasquez <mvelasquez@pictoric.com>
2022-06-21 21:41:19 -0500

    created components templateForm an reactiveForm

--- src/app/reactive-form/reactive-form.module.ts
+++ src/app/reactive-form/reactive-form.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ReactiveFormComponent } from '../components/reactive-form/reactive-form.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ReactiveFormComponent],
+  imports: [CommonModule],
+  exports: [ReactiveFormComponent],
 })
-export class ReactiveFormModule { }
+export class ReactiveFormModule {}
--- src/app/template-form/template-form.module.ts
+++ src/app/template-form/template-form.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { TemplateFormComponent } from '../components/template-form/template-form.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [TemplateFormComponent],
+  imports: [CommonModule],
+  exports: [TemplateFormComponent],
 })
-export class TemplateFormModule { }
+export class TemplateFormModule {}
