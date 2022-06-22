896ab04efdfc0300c5217eba40282e0c4973e832
Moises Velasquez <mvelasquez@pictoric.com>
2022-06-21 23:19:41 -0500

    FormsModule avoids recharging

--- src/app/template-form/template-form.module.ts
+++ src/app/template-form/template-form.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemplateFormComponent } from '../components/template-form/template-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TemplateFormComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [TemplateFormComponent],
 })
 export class TemplateFormModule {}
