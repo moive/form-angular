c393a334cbbd374a56f8c97fe4afda7000a16e5f
Moises Velasquez <mvelasquez@pictoric.com>
2022-07-25 20:57:51 -0500

    testing fields form

--- src/app/components/reactive-form/reactive-form.component.spec.ts
+++ src/app/components/reactive-form/reactive-form.component.spec.ts
@@ -1,26 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
-import { ReactiveFormModule } from 'src/app/reactive-form/reactive-form.module';
 
 import { ReactiveFormComponent } from './reactive-form.component';
 
 describe('ReactiveFormComponent', () => {
   let component: ReactiveFormComponent;
-  let fixture: ComponentFixture<ReactiveFormComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ReactiveFormComponent],
-      imports: [ReactiveFormModule],
-      providers: [FormBuilder],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(ReactiveFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    component = new ReactiveFormComponent(new FormBuilder());
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create a form with 3 fields: project, hours and technologies', () => {
+    const form = component.myForm;
+
+    const project = form.get('project');
+    const hours = form.get('hours');
+    const technologies = form.get('technologies');
+
+    expect(project).toBeTruthy();
+    expect(hours).toBeTruthy();
+    expect(technologies).toBeTruthy();
   });
 });
